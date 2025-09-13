97b3a1305f478878ba7e1911b21d0e6655e52692
Shehryar Aijaz <shehryar120345@gmail.com>
2025-09-14 01:55:43 +0500

    feat: enhance dashboard page with user account fetching, improved account management, and integration of Settings and Footer components

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -3,12 +3,14 @@
 import { Button } from "@/components/ui/button";
 import { useSession } from "next-auth/react";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner"
 import { MdModeEdit } from "react-icons/md";
 import EmailPreview from "@/components/EmailPreview";
 import Schedule from "@/components/Schedule";
-import TopTweets from "@/components/TopTweets";
+import Settings from "@/components/Settings";
+import Footer from "@/components/Footer";
+import axios from "axios";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -20,8 +22,32 @@ export default function Home() {
   });
   const [frequency, setFrequency] = useState<string>("Daily");
   const [topTweets, setTopTweets] = useState<boolean>(false);
+  const [aiSummaries, setAiSummaries] = useState<boolean>(false);
+  
+  console.log(frequency)
 
   const [addButtonClicked, setAddButtonClicked] = useState<boolean>(false);
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      const response = await axios.get('/api/accounts')
+      if (response.data.success) {
+        console.log("response.data.user", response.data.user)
+        setAccounts({
+          account1: response.data.user.socialHandles.accounts[0],
+          account2: response.data.user.socialHandles.accounts[1],
+          account3: response.data.user.socialHandles.accounts[2],
+        })
+        setFrequency(response.data.user.frequency.charAt(0).toUpperCase() + response.data.user.frequency.slice(1))
+        setAddButtonClicked(true)
+      } else {
+        toast.error(response.data.message)
+      }
+    }
+    fetchUserDetails()
+  }, [])
 
   // Helper to update a specific account value
   const handleAccountInputChange = (account: string, value: string) => {
@@ -36,21 +62,37 @@ export default function Home() {
     // Add your save logic here
   };
 
-  const handleAddButtonClick = () => {
+  const handleAddButtonClick = async () => {
     if (Object.values(accounts).includes("")) {
       console.log("button clicked..");
       setAddButtonClicked(false);
-      toast.error("Please add an account before clicking add");
+      toast.error("Please add all accounts before clicking add");
     } else {
       setAddButtonClicked(true);
+      setIsLoading(true)
+
+      try {
+        const response = await axios.post(`/api/accounts`, {
+          account1: accounts.account1,
+          account2: accounts.account2,
+          account3: accounts.account3
+        })
+        if (response.data.success) {
+          toast.success(response.data.message)
+        } else {
+          toast.error(response.data.message)
+        }
+      } catch (error) {
+        toast.error("Error adding accounts")
+      } finally {
+        setIsLoading(false)
+      }
     }
   };
 
   const handleEditButtonClick = () => {
     setAddButtonClicked(false);
   }
-
-  console.log(accounts)
 
   return (
     <div className="flex flex-col justify-center max-w-4xl mx-auto px-6 py-10">
@@ -69,6 +111,7 @@ export default function Home() {
               type="text"
               placeholder="Account 1"
               value={accounts.account1}
+              disabled={addButtonClicked}
             />
           </div>
           <div className="flex items-center gap-2">
@@ -77,6 +120,7 @@ export default function Home() {
               type="text"
               placeholder="Account 2"
               value={accounts.account2}
+              disabled={addButtonClicked}
             />
           </div>
           <div className="flex items-center gap-2">
@@ -85,13 +129,14 @@ export default function Home() {
               type="text"
               placeholder="Account 3"
               value={accounts.account3}
+              disabled={addButtonClicked}
             />
           </div>
         </div>
         <div className="mt-3 flex items-center gap-2">
           <Button
             variant="default"
-            className="h-10"
+            className="h-10 cursor-pointer"
             onClick={handleAddButtonClick}
             disabled={addButtonClicked}
           >
@@ -100,7 +145,7 @@ export default function Home() {
           {addButtonClicked && (
             <Button
               variant="outline"
-              className="h-10 flex items-center justify-center px-3"
+              className="h-10 flex items-center justify-center px-3 cursor-pointer"
               onClick={handleEditButtonClick}
             >
               <MdModeEdit />
@@ -111,10 +156,11 @@ export default function Home() {
 
       <Schedule frequency={frequency} setFrequency={setFrequency} />
 
-      <EmailPreview />
+      <EmailPreview accounts={accounts} />
 
-      <TopTweets topTweets={topTweets} setTopTweets={setTopTweets} />
+      <Settings topTweets={topTweets} setTopTweets={setTopTweets} aiSummaries={aiSummaries} setAiSummaries={setAiSummaries} disabled={true} />
 
+      <Footer />
     </div>
   );
 }
