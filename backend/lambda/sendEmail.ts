8da1d13f607e2c25a3857577ec20e77b29c3cce0
Shehryar Aijaz <shehryar120345@gmail.com>
2025-09-25 12:10:15 +0500

    feat: add Lambda function for sending emails and schedule it with EventBridge; implement cron job for testing; lambda function is in works

--- backend/lambda/sendEmail.ts
+++ backend/lambda/sendEmail.ts
@@ -1,5 +1,24 @@
-export async function handler(event: any) {
-    console.log("Lambda triggered!", event);
-    return { statusCode: 200, body: "Hello from Lambda!" };
-  }
-  
+import { MongoClient } from 'mongodb';
+import fetch from 'node-fetch';
+
+const MONGODB_URI = process.env.MONGODB_URI as string
+const RESEND_API_KEY = process.env.RESEND_API_KEY as string
+const BASE_API_URL = process.env.BASE_API_URL as string
+
+exports.handler = async function (event: any) {
+    const client = new MongoClient(MONGODB_URI)
+    await client.connect()
+    const db = client.db("emaildigest")
+    const user = db.collection("users")
+
+    const allUsers = await user.find({}).toArray();
+    const now = new Date();
+
+    for (const user of allUsers) {
+        const lastSent = user.lastEmailSent ? new Date(user.lastEmailSent) : null;
+        const signupDate = new Date(user.created_at)
+        let sendEmail = false;
+
+        // Determine if email is due
+    }
+}
--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,6 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaNode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +17,20 @@ export class BackendStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'BackendQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // 1. Define Lambda (bundle TypeScript entry)
+    const sendEmailLambda = new lambdaNode.NodejsFunction(this, 'SendEmailLambda', {
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      entry: path.join(__dirname, '..', 'lambda', 'sendEmail.ts'),
+      handler: 'handler',
+    });
+
+    // 2. EventBridge Rule (cron job every minute for testing)
+    const rule = new events.Rule(this, "SendEmailSchedule", {
+      schedule: events.Schedule.expression("cron(* * * * ? *)")
+    });
+
+    // 3. Attach Lambda as Target
+    rule.addTarget(new targets.LambdaFunction(sendEmailLambda))
   }
 }
